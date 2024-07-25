3db23f9444227759473e92ca34cd3532b0060f90
Yash Srivastava <147892993+yash77423@users.noreply.github.com>
2024-07-25 16:12:55 +0530

    Add files via upload

--- CVL/Lab1/l1q6.py
+++ CVL/Lab1/l1q6.py
@@ -13,11 +13,14 @@ rot_img_2 = cv2.rotate(src_img, cv2.ROTATE_180)  # Using cv2.ROTATE_180 rotate b
 rot_img_3 = cv2.rotate(src_img, cv2.ROTATE_90_COUNTERCLOCKWISE)  # Using cv2.ROTATE_90_COUNTERCLOCKWISE rotate by 270 degrees clockwise
 
 # Show the images
-plt.figure(figsize=(18, 5))
-plt.subplot(141); plt.imshow(rot_img_1); plt.title("Rotate 90° CW")
 
-plt.subplot(142); plt.imshow(rot_img_2); plt.title("Rotate 180°")
+cv2.imshow('Rotate 90° CW', rot_img_1)
+cv2.waitKey(0)
+cv2.imshow('Rotate 180°', rot_img_2)
+cv2.waitKey(0)
+cv2.imshow('Rotate 90° ACW', rot_img_3)
+cv2.waitKey(0)
+cv2.imshow('Original', src_img)
+cv2.waitKey(0)
 
-plt.subplot(143); plt.imshow(rot_img_3); plt.title("Rotate 90° ACW")
-
-plt.subplot(144); plt.imshow(src_img); plt.title("Original")
+cv2.destroyAllWindows()
